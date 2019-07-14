2f610c0caac0df3175da060ebce9831a49157f2c
Hasnain_ <hasnainali555@gmail.com>
2019-07-14 19:44:20 +0500

    modified forgetView

--- app/features/forgotPassword/containers/ForgotContainer.js
+++ app/features/forgotPassword/containers/ForgotContainer.js
@@ -1,45 +1,44 @@
-import React, {Component} from 'react';
-import ForgotView from './ForgotView';
-import { ImageBackground, Dimensions, View, Text } from 'react-native'
+import React, { Component } from "react";
+import ForgotView from "./ForgotView";
+import { ImageBackground, Dimensions, View, Text } from "react-native";
 import {
-    Header,
-    Left,
-    Body,
-    Title,
-    Button,
-    Container,
-    Content,
-    Right,
-    Tabs,
-    Tab
-  } from "native-base";
-import NavBarButton from '../../../components/Button/NavBarButton';
-import Ionicons from '@expo/vector-icons/Ionicons';
+  Header,
+  Left,
+  Body,
+  Title,
+  Button,
+  Container,
+  Content,
+  Right,
+  Tabs,
+  Tab
+} from "native-base";
+import NavBarButton from "../../../components/Button/NavBarButton";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default class ForgotContainer extends Component {
-    constructor(props) {
-        super(props);
-        
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return(
-            <ImageBackground style={{flex: 1}} source={require('../../../../assets/images/splash.jpg')}>
-              <Header transparent iosBarStyle="light-content">
-                <Left style={{ flex: 1, paddingLeft: 10 }}>
-                    <Button transparent onPress={()=> this.props.navigation.goBack()}>
-                        <Ionicons name="ios-arrow-back" size={30} color="#fff" />
-                    </Button>
-                </Left>
-                <Body style={{ flex: 1, justifyContent: "center" }}>
-                  
-                </Body>
-                <Right style={{ flex: 1 }}>
-                  
-                </Right>
-              </Header>
-                <ForgotView></ForgotView>
-            </ImageBackground>
-        )
-    }
+  render() {
+    return (
+      <Container style={{ flex: 1, backgroundColor: "#fff" }}>
+        <Header
+          transparent
+          iosBarStyle="light-content"
+          style={{ backgroundColor: "rgb(219,168,65)" }}
+        >
+          <Left style={{ flex: 1, paddingLeft: 10 }}>
+            <Button transparent onPress={() => this.props.navigation.goBack()}>
+              <Ionicons name="ios-arrow-back" size={30} color="#fff" />
+            </Button>
+          </Left>
+          <Body style={{ flex: 1, justifyContent: "center" }} />
+          <Right style={{ flex: 1 }} />
+        </Header>
+        <ForgotView />
+      </Container>
+    );
+  }
 }
--- app/features/forgotPassword/containers/ForgotView.js
+++ app/features/forgotPassword/containers/ForgotView.js
@@ -1,46 +1,79 @@
-import React, {Component} from 'react';
-import {View, Text, Image, TouchableOpacity, TextInput} from 'react-native';
-import styles from './styles';
-import InputField from '../../login/components/TextInput';
+import React, { Component } from "react";
+import { View, Text, Image, TouchableOpacity, TextInput } from "react-native";
+import styles from "./styles";
+import InputField from "../../login/components/TextInput";
+import { Content } from "native-base";
 
 export default class ForgotView extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            screenIndex: 0
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      screenIndex: 0
+    };
 
-        this.renderEmailScreen = this.renderEmailScreen.bind(this);
-    }
+    this.renderEmailScreen = this.renderEmailScreen.bind(this);
+  }
 
-    render() {
-        let {container} = styles;
-        return(
-            <View style = {container} >
-                {this.state.screenIndex == 0 && this.renderEmailScreen() }
-            </View>
-        )
-    }
+  render() {
+    let { container } = styles;
+    return (
+      <View style={{ flex: 1 }}>
+        {this.state.screenIndex == 0 && this.renderEmailScreen()}
+      </View>
+    );
+  }
 
-    renderEmailScreen() {
-        let {headerText, image, instructions, continueButton, signInText} = styles;
-        return(
-            <View>
-                <Text style = {[headerText,{color: '#fff'}]} >Forget Password</Text>
-                    <Text style = {{color: 'white', alignSelf: 'center', fontSize: 30, marginTop: 40}}>UNDISPUTED FLAG FOOTBALL LEAGUE</Text>
-                <Text style = {[instructions, {marginTop: 20}]}>Please enter the email address you</Text>
-                <Text style = {[instructions, {alignSelf: 'center'}]} >used to register with.</Text>
-                <Text style = {instructions}>{"A link will be sent to you to reset."}</Text>
-                <TextInput
-                    placeholder= 'Enter your email'
-                    style = {{width: 250, fontSize: 20, padding: 5, alignSelf: 'center', backgroundColor: '#F1F2F6', marginTop: 40, height: 50, borderRadius: 10}}
-                    placeholderTextColor= '#000000'
-                />
-                <TouchableOpacity activeOpacity={0.7} style={continueButton} >
-                    <Text style={signInText}>Continue</Text>
-                </TouchableOpacity>
-            </View>
-        )
-    }
+  renderEmailScreen() {
+    let {
+      headerText,
+      image,
+      instructions,
+      continueButton,
+      signInText
+    } = styles;
+    return (
+      <Content>
+        <View style={styles.backgroundContainer} />
+        <View style={[styles.container, { paddingTop: 50, paddingBottom: 20 }]}>
+          <Text style={[headerText, { color: "#333" }]}>Forget Password</Text>
+          <Text
+            style={{
+              color: "#333",
+              textAlign: "center",
+              fontSize: 25,
+              marginTop: 40
+            }}
+          >
+            UNDISPUTED FLAG FOOTBALL LEAGUE
+          </Text>
+          <Text style={[instructions, { marginTop: 20, color: "#333" }]}>
+            Please enter the email address you
+          </Text>
+          <Text style={[instructions, { alignSelf: "center", color: "#333" }]}>
+            used to register with.
+          </Text>
+          <Text style={instructions}>
+            {"A link will be sent to you to reset."}
+          </Text>
+          <TextInput
+            placeholder="Enter your email"
+            style={{
+              width: 250,
+              fontSize: 20,
+              padding: 5,
+              alignSelf: "center",
+              backgroundColor: "#F1F2F6",
+              marginTop: 40,
+              height: 50,
+              borderRadius: 10
+            }}
+            placeholderTextColor="#000000"
+          />
+          <TouchableOpacity activeOpacity={0.7} style={continueButton}>
+            <Text style={signInText}>Continue</Text>
+          </TouchableOpacity>
+        </View>
+      </Content>
+    );
+  }
 }
